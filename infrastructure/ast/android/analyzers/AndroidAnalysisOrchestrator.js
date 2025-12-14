04746335c456bdfb8ba79d1fcfd13531940a4cdb
Swift En Profundidad <freelancemerlos@gmail.com>
2025-12-14 02:28:02 +0100

    refactor: Completar refactor de AndroidASTIntelligentAnalyzer
    - Usar AndroidASTParser para extracción
    - Usar AndroidClassAnalyzer para análisis SOLID
    - Mantener análisis restante en archivo original temporal
    - Wrapper AndroidASTIntelligentAnalyzer delega a orquestador
    
    Reduce complejidad de 774 líneas a módulos más pequeños (SRP).
    
    🐈💚 Pumuki Team® - SOLID Principles

--- infrastructure/ast/android/analyzers/AndroidAnalysisOrchestrator.js
+++ infrastructure/ast/android/analyzers/AndroidAnalysisOrchestrator.js
@@ -1,6 +1,7 @@
 const { AndroidASTParser } = require('./AndroidASTParser');
 const { AndroidClassAnalyzer } = require('./AndroidClassAnalyzer');
-const AndroidOriginalAnalyzer = require('./AndroidOriginalAnalyzer');
+
+const OriginalAnalyzer = require('./AndroidASTIntelligentAnalyzerOriginal');
 
 class AndroidAnalysisOrchestrator {
     constructor(findings) {
@@ -16,8 +17,26 @@ class AndroidAnalysisOrchestrator {
         const classAnalyzer = new AndroidClassAnalyzer(parser, this.findings);
         classAnalyzer.analyze();
 
-        const originalAnalyzer = new AndroidOriginalAnalyzer(parser, this.findings);
-        originalAnalyzer.analyzeAll();
+        const originalAnalyzer = new OriginalAnalyzer.AndroidASTIntelligentAnalyzer(this.findings);
+        originalAnalyzer.content = parser.content;
+        originalAnalyzer.lines = parser.lines;
+        originalAnalyzer.filePath = parser.filePath;
+        originalAnalyzer.imports = parser.imports;
+        originalAnalyzer.classes = parser.classes;
+        originalAnalyzer.functions = parser.functions;
+        originalAnalyzer.annotations = parser.annotations;
+
+        originalAnalyzer.analyzeCompose();
+        originalAnalyzer.analyzeCoroutines();
+        originalAnalyzer.analyzeFlow();
+        originalAnalyzer.analyzeHilt();
+        originalAnalyzer.analyzeRoom();
+        originalAnalyzer.analyzeSecurity();
+        originalAnalyzer.analyzeCleanArchitecture();
+        originalAnalyzer.analyzeAntiPatterns();
+        originalAnalyzer.analyzeNullSafety();
+        originalAnalyzer.analyzeImportsRules();
+        originalAnalyzer.analyzeAdditionalRules();
     }
 }
 
