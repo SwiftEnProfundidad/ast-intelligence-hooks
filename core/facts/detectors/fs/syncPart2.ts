84913cac81228bfcdb15a47bc942636c28eda032
Juan Carlos Merlos Albarracín <freelancemerlos@gmail.com>
2026-02-15 16:18:47 +0100

    refactor(fs): extract syncPart2 permissions detectors

--- core/facts/detectors/fs/syncPart2.ts
+++ core/facts/detectors/fs/syncPart2.ts
@@ -1,5 +1,7 @@
 import { hasNode, isObject } from '../utils/astHelpers';
 
+export * from './syncPart2Permissions';
+
 export const hasFsUnlinkSyncCall = (node: unknown): boolean => {
   return hasNode(node, (value) => {
     if (value.type !== 'CallExpression') {
@@ -65,98 +67,6 @@ export const hasFsRmdirSyncCall = (node: unknown): boolean => {
       isObject(propertyNode) &&
       propertyNode.type === 'Identifier' &&
       propertyNode.name === 'rmdirSync'
-    );
-  });
-};
-
-export const hasFsChmodSyncCall = (node: unknown): boolean => {
-  return hasNode(node, (value) => {
-    if (value.type !== 'CallExpression') {
-      return false;
-    }
-
-    const callee = value.callee;
-    if (!isObject(callee) || callee.type !== 'MemberExpression' || callee.computed === true) {
-      return false;
-    }
-    const objectNode = callee.object;
-    const propertyNode = callee.property;
-    return (
-      isObject(objectNode) &&
-      objectNode.type === 'Identifier' &&
-      objectNode.name === 'fs' &&
-      isObject(propertyNode) &&
-      propertyNode.type === 'Identifier' &&
-      propertyNode.name === 'chmodSync'
-    );
-  });
-};
-
-export const hasFsChownSyncCall = (node: unknown): boolean => {
-  return hasNode(node, (value) => {
-    if (value.type !== 'CallExpression') {
-      return false;
-    }
-
-    const callee = value.callee;
-    if (!isObject(callee) || callee.type !== 'MemberExpression' || callee.computed === true) {
-      return false;
-    }
-    const objectNode = callee.object;
-    const propertyNode = callee.property;
-    return (
-      isObject(objectNode) &&
-      objectNode.type === 'Identifier' &&
-      objectNode.name === 'fs' &&
-      isObject(propertyNode) &&
-      propertyNode.type === 'Identifier' &&
-      propertyNode.name === 'chownSync'
-    );
-  });
-};
-
-export const hasFsFchownSyncCall = (node: unknown): boolean => {
-  return hasNode(node, (value) => {
-    if (value.type !== 'CallExpression') {
-      return false;
-    }
-
-    const callee = value.callee;
-    if (!isObject(callee) || callee.type !== 'MemberExpression' || callee.computed === true) {
-      return false;
-    }
-    const objectNode = callee.object;
-    const propertyNode = callee.property;
-    return (
-      isObject(objectNode) &&
-      objectNode.type === 'Identifier' &&
-      objectNode.name === 'fs' &&
-      isObject(propertyNode) &&
-      propertyNode.type === 'Identifier' &&
-      propertyNode.name === 'fchownSync'
-    );
-  });
-};
-
-export const hasFsFchmodSyncCall = (node: unknown): boolean => {
-  return hasNode(node, (value) => {
-    if (value.type !== 'CallExpression') {
-      return false;
-    }
-
-    const callee = value.callee;
-    if (!isObject(callee) || callee.type !== 'MemberExpression' || callee.computed === true) {
-      return false;
-    }
-    const objectNode = callee.object;
-    const propertyNode = callee.property;
-    return (
-      isObject(objectNode) &&
-      objectNode.type === 'Identifier' &&
-      objectNode.name === 'fs' &&
-      isObject(propertyNode) &&
-      propertyNode.type === 'Identifier' &&
-      propertyNode.name === 'fchmodSync'
     );
   });
 };
@@ -321,4 +231,3 @@ export const hasFsWriteSyncCall = (node: unknown): boolean => {
     );
   });
 };
-
