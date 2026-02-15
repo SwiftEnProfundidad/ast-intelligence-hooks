b575d404b5333d036674da4bb0897c33fc9d82dd
Juan Carlos Merlos Albarracín <freelancemerlos@gmail.com>
2026-02-15 13:39:51 +0100

    test(infra): recover jest typescript execution and coverage signal

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,21 @@
 module.exports = {
     bail: true,
     testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.spec.js', '**/__tests__/**/*.spec.js'],
-    transform: {},
+    testMatch: ['**/__tests__/**/*.spec.ts'],
+    transform: {
+        '^.+\\.tsx?$': 'babel-jest'
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/legacy/'],
     modulePathIgnorePatterns: ['<rootDir>/.ast-intelligence/'],
     collectCoverage: true,
-    collectCoverageFrom: ['application/services/notification/**/*.js'],
+    collectCoverageFrom: [
+        'core/**/*.ts',
+        'integrations/**/*.ts',
+        '!**/__tests__/**',
+        '!**/*.test.ts',
+        '!**/*.spec.ts'
+    ],
     coverageDirectory: '.coverage',
     reporters: ['default'],
     setupFilesAfterEnv: []
